415352ff17bcd36983a755cf5ed078ad2ac965ef
Maksym Yarmolenko <66525757+yarmmak@users.noreply.github.com>
2024-02-13 22:14:02 +0200

    Updated JSON Parser

--- task10/jsrc/main/java/com/task10/strategy/impl/RetrieveReservationsApiStrategy.java
+++ task10/jsrc/main/java/com/task10/strategy/impl/RetrieveReservationsApiStrategy.java
@@ -2,7 +2,11 @@ package com.task10.strategy.impl;
 
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import com.google.gson.ExclusionStrategy;
+import com.google.gson.FieldAttributes;
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.task10.model.ReservationsModel;
 import com.task10.service.DynamoDbClient;
 import com.task10.strategy.ApiStrategy;
 
@@ -20,13 +24,29 @@ public class RetrieveReservationsApiStrategy implements ApiStrategy
     @Override
     public APIGatewayProxyResponseEvent processApi(final APIGatewayProxyRequestEvent input)
     {
+        Gson gsonWithExclusion = new GsonBuilder()
+                .setExclusionStrategies(new ExcludeIdFieldStrategy())
+                .create();
+
         return new APIGatewayProxyResponseEvent()
-                .withBody(gson.toJson(dbClient.getAllReservations()));
+                .withBody(gsonWithExclusion.toJson(dbClient.getAllReservations()));
     }
 
     @Override
     public boolean isApplicable(final String path, final String method)
     {
         return "/reservations".equals(path) && GET_REQUEST.equals(method);
+    }
+
+    public static class ExcludeIdFieldStrategy implements ExclusionStrategy {
+        @Override
+        public boolean shouldSkipField(FieldAttributes f) {
+            return f.getDeclaringClass() == ReservationsModel.class && f.getName().equals("id");
+        }
+
+        @Override
+        public boolean shouldSkipClass(Class<?> clazz) {
+            return false;
+        }
     }
 }
